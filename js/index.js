7c1aacc9290ef4c207cfee60158018a04546a54c
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-03-13 22:44:10 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,9 +14,15 @@ var getQuote = function() {
     fetchData('https://raw.githubusercontent.com/asozialesnetzwerk/zitate/master/zitate.txt').then(arr => {
         $(".quote-text").text(arr[Math.floor(Math.random() * arr.length)]);
     } );
+	
+ $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent($(".quote-text").text()+ ' ' +  $(".quote-author").text()));
+
     
   // $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + data.quoteText + '"' + ' - ' +  data.quoteAuthor));
 };
+
+$("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent($(".quote-text").text()+ ' ' +  $(".quote-author").text()));
+
 
 // $(".tweet").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + firstQuote + '"' + ' - ' + firstAuthor));
 
