c929c6fa2ef6767f401c6c2c96f6a62a9c841e1a
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-03-14 12:08:50 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ function displayZitat(id) {
     var theQuote = window.q[1][ids[0]];
     var theAuthor = window.q[0][ids[1]];
     var rating = window.r[0][id];
-    if(rating == undefined) rating = "0";
+    if(rating === undefined) rating = "0";
     
     $(".quote-text").text(theQuote);
     $(".quote-author").text("- " + theAuthor);
