450280beef610b281c710d3d3f20842697535aab
Christian Berg <cberg@stud.hs-heilbronn.de>
2020-05-23 03:16:11 +0200

    another rating..

--- info/index.js
+++ info/index.js
@@ -1,4 +1,54 @@
+var rating = "https://raw.githubusercontent.com/asozialesnetzwerk/zitate/master/bewertung_zitate.json"
+var authors = "https://raw.githubusercontent.com/asozialesnetzwerk/zitate/master/namen.txt"
+var quotes = "https://raw.githubusercontent.com/asozialesnetzwerk/zitate/master/zitate.txt"
+
+window.q = [];
+window.r = [];
+
+var getQuote = function(data) {
+    q.push(data.split(/\n/));
+    if(window.q.length==2 && window.r.length==1) createZitat();
+};
+var getRating = function(data) {
+    r.push(JSON.parse(data));
+    if(window.q.length==2 && window.r.length==1) createZitat();
+};
+
+$.get(authors, getQuote, 'text');
+$.get(quotes, getQuote, 'text');
+$.get(rating, getRating, 'text');
+
+$(".get-quote").on("click", function() {
+    createZitat();
+});
+
+function createZitat()
+{
+    var a = Math.floor(Math.random() * window.q[0].length);
+    var q = Math.floor(Math.random() * window.q[1].length);
+    var z = q+"-"+a; //Zitat-ID
+    
+    $(".quote-id").text(z);
+    $(".quote-rating").text("Rating: "+window.r[0][z]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var author="579"
+
 
 //alert(ratingJson);
 alert("HI");
