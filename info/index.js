8073d0a1f9106c8de0fe0e9c05e2d8d94ca9ea7a
Christian Berg <cberg@stud.hs-heilbronn.de>
2020-05-23 03:37:06 +0200

    author from array

--- info/index.js
+++ info/index.js
@@ -1,76 +1,16 @@
 var rating = "https://raw.githubusercontent.com/asozialesnetzwerk/zitate/master/bewertung_zitate.json"
-var inputfield = "2271-656";
-
-window.q = [];
-window.r = [];
-
-var getQuote = function(data) {
-    q.push(data.split(/\n/));
-    if(window.q.length==2 && window.r.length==1) createZitat();
-};
-var getRating = function(data) {
-    r.push(JSON.parse(data));
-    if(window.q.length==2 && window.r.length==1) createZitat();
-};
-
-$.get(rating, getRating, 'text');
-
-$(".get-quote").on("click", function() {
-    createZitat();
-});
-
-function createZitat()
-{
-    var a = Math.floor(Math.random() * window.q[0].length);
-    var q = Math.floor(Math.random() * window.q[1].length);
-    var z = inputfield; //Zitat-ID
-    
-    $(".quote-id").text(z);
-    $(".quote-rating").text("Rating: "+window.r[0][z]);
-}
-
-function myQuoteStats() {
-  inputfield = document.getElementById("myQuote").value;
-  alert(inputfield);
-  createZitat()
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 var author="579"
 
+alert(rating);
 
-//alert(ratingJson);
-alert("HI");
-
-var arr =
-{"1075-162":-1,
-"184-304":0,
-"837-765":1,
-"1798-17":-1,
-"1387-579":-1,
-"2624-790":1,
-"LOL-579":7,
-"2694-523":0,
-"492-498":0,
-"12-579":7,
-"1404-517":0}
+document.getElementById("myRating").textContent=rating;
 
 
-var keys = Object.keys(arr);
+var keys = Object.keys(rating);
 console.log(keys)
 
 console.log(keys.filter(s=>~s.indexOf("-"+author)));
@@ -78,7 +18,7 @@ var rip = keys.filter(s=>~s.indexOf("-"+author)); //contain author
 
 var newarr = {};
 for (let [key, value] of Object.entries(rip)) {
-    newarr[`"${value}"`] =arr[`${value}`];
+    newarr[`"${value}"`] =rating[`${value}`];
 }
 
 console.log(newarr);
